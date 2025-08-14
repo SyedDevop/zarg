a219d88a39eb53ceb697b7e62343d6515f50ffd0
SyedDevop <syeds.devops007@gmail.com>
2025-08-15 01:44:47 +0530

    wip: win errors

--- src/term/win_util.zig
+++ src/term/win_util.zig
@@ -4,43 +4,47 @@ const win = os.windows;
 const winK = win.kernel32;
 
 // input
-pub const ENABLE_PROCESSED_INPUT: u32 = 0x0001;
-pub const ENABLE_LINE_INPUT: u32 = 0x0002;
-pub const ENABLE_ECHO_INPUT: u32 = 0x0004;
-pub const ENABLE_VIRTUAL_TERMINAL_INPUT: u32 = 0x0200;
+pub const ENABLE_PROCESSED_INPUT = 0x0001;
+pub const ENABLE_LINE_INPUT = 0x0002;
+pub const ENABLE_ECHO_INPUT = 0x0004;
+pub const ENABLE_VIRTUAL_TERMINAL_INPUT = 0x0200;
 // OutPUT
-pub const ENABLE_PROCESSED_OUTPUT: u32 = 0x0001;
-pub const ENABLE_WRAP_AT_EOL_OUTPUT: u32 = 0x0002;
-pub const ENABLE_VIRTUAL_TERMINAL_PROCESSING: u32 = 0x0004;
-pub const DISABLE_NEWLINE_AUTO_RETURN: u32 = 0x0008;
+pub const ENABLE_PROCESSED_OUTPUT = 0x0001;
+pub const ENABLE_WRAP_AT_EOL_OUTPUT = 0x0002;
+pub const ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+pub const DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
 
 pub fn getConsoleMode(handle: win.HANDLE) !win.DWORD {
     var mode: win.DWORD = undefined;
-    return switch (winK.GetConsoleMode(handle, &mode)) {
-        else => return mode,
-        // win.FALSE => {
-        // const err = winK.GetLastError();
-        // return win.unexpectedError(err);
-        // },
-        win.FALSE => {
-            return switch (winK.GetLastError()) {
-                else => |e| win.unexpectedError(e),
-            };
-        },
+    if (winK.GetConsoleMode(handle, &mode) == 0) switch (winK.GetLastError()) {
+        else => |e| return win.unexpectedError(e),
     };
+    // // win.FALSE => {
+    // // const err = winK.GetLastError();
+    // // return win.unexpectedError(err);
+    // // },
+    // win.FALSE => {
+    //     return switch (winK.GetLastError()) {
+    //         else => |e| win.unexpectedError(e),
+    //     };
+    // },
+    // }
+    return mode;
 }
 pub fn setConsoleMode(handle: win.HANDLE, mode: win.DWORD) !void {
-    switch (winK.SetConsoleMode(handle, mode)) {
-
-        // win.FALSE => {
-        // const err = winK.GetLastError();
-        // return win.unexpectedError(err);
-        // },
-        win.FALSE => {
-            return switch (winK.GetLastError()) {
-                else => |e| win.unexpectedError(e),
-            };
-        },
-        else => {},
-    }
+    if (winK.SetConsoleMode(handle, mode)) switch (winK.GetLastError()) {
+        else => |e| return win.unexpectedError(e),
+    };
+    // {
+    //         // win.FALSE => {
+    //         // const err = winK.GetLastError();
+    //         // return win.unexpectedError(err);
+    //         // },
+    //         win.FALSE => {
+    //             return switch (winK.GetLastError()) {
+    //                 else => |e| win.unexpectedError(e),
+    //             };
+    //         },
+    //         else => {},
+    //     }
 }
